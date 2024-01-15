154c63fa7cf793e9ac44682900effd1b8906030b
Stoichitoiu Damian <100224851+StoichitoiuDamian@users.noreply.github.com>
2024-01-15 19:31:38 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,18 +1,46 @@
 function authenticate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    var message = document.getElementById("message");
+
+    // Resetează mesajul de eroare
+    closeErrorModal();
+
+    // Verifică dacă câmpurile sunt goale
+    if (username.trim() === "" || password.trim() === "") {
+        openErrorModal("Completați toate câmpurile!");
+        return;
+    }
 
     // Verifică credențialele hardcodate
     if (username === "parinte" && password === "parinte") {
-        message.innerHTML = "Autentificare reușită pentru părinte!";
-        // Redirecționează către pagina pentru configurarea programului de iluminat pentru părinte
         window.location.href = "pagina_parinte.html";
     } else if (username === "copil" && password === "copil") {
-        message.innerHTML = "Autentificare reușită pentru copil!";
-        // Redirecționează către pagina pentru configurarea programului de iluminat pentru copil
         window.location.href = "pagina_copil.html";
     } else {
-        message.innerHTML = "Credențiale incorecte. Încercați din nou.";
+        openErrorModal("Username or password are wrong. Please try again.");
     }
+}
+
+function openErrorModal(message) {
+    var errorModal = document.getElementById("errorModal");
+    var errorMessageModal = document.getElementById("error-message-modal");
+
+    errorMessageModal.innerHTML = message;
+    errorModal.style.display = "flex";
+
+    // Închide fereastra modală după 2000 de milisecunde (2 secunde)
+    setTimeout(function () {
+        closeErrorModal();
+    }, 2000);
+}
+
+function closeErrorModal() {
+    var errorModal = document.getElementById("errorModal");
+    errorModal.style.display = "none";
+}
+
+
+function logout() {
+    // Redirecționează către pagina de login
+    window.location.href = "login.html";
 }
